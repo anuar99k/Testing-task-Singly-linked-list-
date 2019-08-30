2828f99401d7cb08736ab5f93ea9e6e559e7a3ed
Ануар Кабылбаев <kabylbaevanuar@gmail.com>
2019-08-30 13:57:44 +0600

    Add project files.

---
+++ Testing task 'Singly linked list'/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_task__Singly_linked_list_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SinglyLinkedList<int> numbers = new SinglyLinkedList<int>();
+                numbers.Add(1);
+                numbers.Insert(0, 0);
+                numbers.Insert(2, 2);
+                numbers.Add(3);
+                numbers.Add(4);
+                numbers.Add(5);
+                numbers.RemoveAt(3);
+                Console.WriteLine($"Number of elements: {numbers.Count}");
+                foreach (int i in numbers)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine($"Second element is: {numbers.GetByIndex(1)}");
+                Console.WriteLine("Odd numbers:");
+                //LINQ query for getting odd numbers from the list
+                var oddNumbers = from num in numbers
+                           where (num % 2) == 1
+                           select num;
+                foreach (int num in oddNumbers)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Testing task 'Singly linked list'/SinglyLinkedList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Testing_task__Singly_linked_list_
+{
+    class Node<U>
+    {
+        public U data;
+        public Node<U> next;
+        public Node(U Data)
+        {
+            data = Data;
+            next = null;
+        }
+    }
+    class SinglyLinkedList<T> : IEnumerable<T>
+    {
+        private Node<T> _head;
+        public int Count { get; private set; } = 0;
+        public void Add(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (_head == null)
+            {
+                _head = newNode;
+                Count = 1;
+                return;
+            }
+            Node<T> lastNode = GetLastNode();
+            lastNode.next = newNode;
+            Count++;
+        }
+        private Node<T> GetLastNode()
+        {
+            Node<T> LastNode = _head;
+            while (LastNode.next != null)
+                LastNode = LastNode.next;
+            return LastNode;
+        }
+        public void Insert(int index, T data)
+        {
+            if (index < 0 || index > Count)
+                throw new IndexOutOfRangeException();
+            else if (index == Count)
+            {
+                Add(data);
+                return;
+            }
+            Node<T> newNode = new Node<T>(data);
+            if (index == 0)
+            {
+                newNode.next = _head;
+                _head = newNode;
+            }
+            else
+            {
+                Node<T> prevNode = _head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    prevNode = prevNode.next;
+                }
+                newNode.next = prevNode.next;
+                prevNode.next = newNode;
+            }
+            Count++;
+        }
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new IndexOutOfRangeException();
+            else if (index == 0)
+                _head = _head.next;
+            else
+            {
+                Node<T> prevNode = _head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    prevNode = prevNode.next;
+                }
+                prevNode.next = prevNode.next.next;
+            }
+            Count--;
+        }
+        public T GetByIndex(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new IndexOutOfRangeException();
+            else if (index == 0)
+                return _head.data;
+            Node<T> temp = _head.next;
+            for (int i = 1; i < index; i++)
+            {
+                temp = temp.next;
+            }
+            return temp.data;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = _head;
+            while (current != null)
+            {
+                yield return current.data;
+                current = current.next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
